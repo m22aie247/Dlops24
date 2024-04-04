87dc776c7d93ef37c6f5cb80fb90d7ff97722dbc
m22aie247 <166044548+m22aie247@users.noreply.github.com>
2024-04-04 22:37:44 +0530

    2.1 addeed sigmoid for given values

--- activation_function.py
+++ activation_function.py
@@ -15,21 +15,21 @@ def tanhFunction(x):
     return np.tanh(x)
 
 
-x = np.linspace(-5, 5, 100)
-
+# x = np.linspace(-5, 5, 100)
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 # finding the y values 
 y_sigmoid = sigmoidFunction(x)
-y_relu = reluFunction(x)
-y_leaky_relu = leaky_reluFunction(x)
-y_tanh = tanhFunction(x)
+# y_relu = reluFunction(x)
+# y_leaky_relu = leaky_reluFunction(x)
+# y_tanh = tanhFunction(x)
 
 # Plot the graph for activation functions
 plt.figure(figsize=(10, 6))
 
 plt.plot(x, y_sigmoid, label='Sigmoid')
-plt.plot(x, y_relu, label='ReLU')
-plt.plot(x, y_leaky_relu, label='Leaky ReLU')
-plt.plot(x, y_tanh, label='Tanh')
+# plt.plot(x, y_relu, label='ReLU')
+# plt.plot(x, y_leaky_relu, label='Leaky ReLU')
+# plt.plot(x, y_tanh, label='Tanh')
 
 plt.xlabel('Input')
 plt.ylabel('Output')
